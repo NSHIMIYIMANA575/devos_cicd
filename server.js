a27f98fa7b3bac5ceb856ea57ad516f52b2212fa
NSHIMIYIMANA575 <nshanast@gmail.com>
2024-11-15 12:34:16 +0200

    setted

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require("express");
 app.get('/',(rep,res)=>{
     res.sent(" hello world");
 
-})app.listen(3000, ()=>{
+});
+
+app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 });
