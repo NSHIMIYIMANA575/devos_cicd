b4f712678795156041bcc34d3601c0e95755ed9f
NSHIMIYIMANA575 <nshanast@gmail.com>
2024-11-15 14:15:06 +0200

    Update server.js
    good

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express=require("express");
+
 app.get('/',(rep,res)=>{
     res.sent(" hello world");
 
 });
+
+app.get('/users',(rep,res)=>{
+    res.sent(" this is users router");
+
+});
+
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
